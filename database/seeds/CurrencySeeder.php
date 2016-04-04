f1de7c2fb139f5bc2e9016d792227f383099ccc3
hitz <hitz.game@gmail.com>
2016-04-04 13:35:24 +0300

    added copyrights

--- database/seeds/CurrencySeeder.php
+++ database/seeds/CurrencySeeder.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Currencies package by HitzMedia
+ *
+ * (c) Vitaly Drozhdin <purrpryde@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HitzMedia\Currency\Seeds;
 
 use Carbon\Carbon;
